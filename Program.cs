9d6ca34b2c4b8a791d2dc641d24baa7cd085f962
Samson Amaugo <sammychinedu2ky@gmail.com>
2022-02-01 13:40:51 +0100

    added comments

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿using System.Reflection;
 using System.Text.Json;
 
+//call log method
 WeatherLogger.Log("Lagos,Nigeria");
 
 
+//Define Accepted Temperature Units
 enum Units
 {
     Celsius,
@@ -11,6 +13,7 @@ enum Units
 }
 
 
+// Create Attritube constraining it to only Properties
 [AttributeUsage(AttributeTargets.Property)]
 class UnitAttribute : Attribute
 {
@@ -21,11 +24,12 @@ class UnitAttribute : Attribute
     }
 }
 
-
+//Creating Weather Class
 class Weather
 {
     public string country { get; set; }
     public string region { get; set; }
+    //Applying the Attribute to the Temperature Property
     [Unit(Units.Fahrenheit)]
     public double Temperature { get; set; }
 
@@ -36,6 +40,7 @@ class WeatherLogger
     public static string apiKey = "c59654e9053c4be586d212935223101";
     public static void Log(string city)
     {
+        //Trying to obtain the value Defined in the Unit Attribute  from the Temperature Property
         PropertyInfo propertyInfo = typeof(Weather).GetProperty(nameof(Weather.Temperature));
         UnitAttribute  unitAttribute = (UnitAttribute)Attribute.GetCustomAttribute(propertyInfo, typeof(UnitAttribute));
         unitAttribute ??= new UnitAttribute();
@@ -46,15 +51,16 @@ class WeatherLogger
         var region = JsonDocument.Parse(info).RootElement.GetProperty("location").GetProperty("region").ToString();
         var tempC = Convert.ToDouble(JsonDocument.Parse(info).RootElement.GetProperty("current").GetProperty("temp_c").ToString());
         var tempF = Convert.ToDouble(JsonDocument.Parse(info).RootElement.GetProperty("current").GetProperty("temp_f").ToString());
+       
         var newWeather = JsonSerializer.Serialize<Weather>(new Weather()
         {
             region = region,
             country = country,
+            //Deciding which temperature unit to display based on the set unitattribute value
             Temperature = unitAttribute.Unit == Units.Celsius ? tempC : tempF,
         });
         Console.WriteLine(newWeather);
-        
-     
+         
     }
 
 }
